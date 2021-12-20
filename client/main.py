5345938149fa3fad2c6fd66de7b812bfbd4171eb
ranon rat <66473662+ranon-rat@users.noreply.github.com>
2021-12-20 01:26:20 -0600

    Update main.py

--- client/main.py
+++ client/main.py
@@ -1,22 +1,32 @@
-from flux_led import WifiLedBulb
-from websocket import WebSocketApp
+import asyncio
+import pprint
+
+from flux_led.aio import AIOWifiLedBulb
+
+from websocket import create_connection
 # here you should put the private ip of your light
 # so here is  the private ip of my light 
 ip = "192.168.100.59"
-bulb = WifiLedBulb(ip)
 url="ws://ranon-bulb.herokuapp.com/color"
-def hex_to_rgb(hex):
-    return int(hex[1:3], 16), int(hex[3:5], 16), int(hex[5:7], 16)
-def on_message(_, message):
-    print(message)
-    (r,g,b)=hex_to_rgb(message)
-    bulb.set_levels(r,g,b)
 
-def main():
+def hex_to_rgb(hex):
+    if len(hex)<7:
+        hex="#ffffff"
+    return int(hex[1:3], 16), int(hex[3:5], 16), int(hex[5:7], 16)
+
+
+async def main():
+    bulb = AIOWifiLedBulb(ip)
+
+    await bulb.async_setup(lambda:pprint.pprint(["State Changed!", bulb.raw_state]))
+    ws =create_connection(url)
+    print("connected")
+    while(True):
+        result = ws.recv()
+        print(result)
+        (r,g,b)=hex_to_rgb(result)
+        await bulb.async_set_levels(r,g,b)
     
-    ws = WebSocketApp(url, on_message=on_message,on_open=lambda _: print("connected"))
-    ws.run_forever()
     
 if __name__ == "__main__":
-    
-    main()
+    asyncio.run(main())
